a851aca143b30b01ed852fc61bb7a9e03a174883
Pablo Manuel Fernández Velázquez <163730673+ProfitCoder@users.noreply.github.com>
2026-03-26 09:00:34 +0100

    Update GestorEcoCity.java

--- Examen 5/GestorEcoCity.java
+++ Examen 5/GestorEcoCity.java
@@ -3,7 +3,7 @@
 package EcoCity;
 
 public class GestorEcoCity{
-	public static void main(String[] args){
+	public static void main(String [] args){
 		//Declaración de variables y objetos
 		Vehiculo[] vehiculos = new Vehiculo[4];
 		
